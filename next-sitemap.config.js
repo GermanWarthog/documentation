f4d23e6a86d738b08b90412c72eea7a2bdb9d461
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2023-06-07 13:19:18 +0200

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    // siteUrl: 'https://overextended.dev/',
-    // generateRobotsTxt: true,
+    siteUrl: 'https://cmodcraft-studios.eu',
+    generateRobotsTxt: true,
 };
